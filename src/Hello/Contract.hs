cfaa76fcafbc191f761dbe0ff0fe6bd46deb44b7
Frances Disarro <frances.disarra@gmail.com>
2023-01-14 16:58:37 +0000

    work

--- src/Hello/Contract.hs
+++ src/Hello/Contract.hs
@@ -10,11 +10,10 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# Language OverloadedStrings #-}
+{-# LANGUAGE DerivingStrategies #-}
+module Hello.Contract where
 
-
-module Hello.Contract (validator, wrapped, serialized, hash, HelloDatum (..), HelloRedeemer (..)) where
-
-import Cardano.Api.Shelley (PlutusScript (..), PlutusScriptV1)
 import Codec.Serialise (serialise)
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Short as BSS
@@ -24,6 +23,26 @@ import Plutus.V2.Ledger.Api (ScriptContext)
 import PlutusTx
 import PlutusTx.Prelude
 import Cardano.Api
+import Plutus.V1.Ledger.Api (POSIXTime, Address(..))
+import qualified Plutus.V2.Ledger.Contexts as Contexts
+import qualified Plutus.V2.Ledger.Api as PlutusV2
+import Plutus.V2.Ledger.Api (Credential(..))
+import Plutus.V1.Ledger.Interval
+import Cardano.Api.Shelley (PlutusScript(..))
+import Plutus.V2.Ledger.Api (TxInfo)
+import Plutus.V2.Ledger.Contexts (findDatum)
+
+
+{-# INLINEABLE mustFindScriptDatum #-}
+mustFindScriptDatum :: (UnsafeFromData d) => PlutusV2.TxOut -> TxInfo -> d
+mustFindScriptDatum o info = case PlutusV2.txOutDatum o of
+  -- inline datum 
+  PlutusV2.OutputDatum (PlutusV2.Datum dat) -> PlutusTx.unsafeFromBuiltinData dat
+  -- datum hash
+  PlutusV2.OutputDatumHash dh -> case findDatum dh info of
+    Just (PlutusV2.Datum dat) -> PlutusTx.unsafeFromBuiltinData dat
+    _ -> error ()
+  _ -> error ()
 
 newtype HelloDatum = HelloDatum Integer
 PlutusTx.unstableMakeIsData ''HelloDatum
@@ -39,6 +58,9 @@ runWithoutContext (HelloDatum datum) (HelloRedeemer redeemer) = redeemer < datum
 
 -- Entry
 
+-- mkValidator :: HelloRedeemer -> HelloDatum -> ScriptContext -> () 
+-- mkValidator = 
+
 wrappedWithoutContext :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedWithoutContext datum redeemer _ = check $ runWithoutContext (unsafeFromBuiltinData datum) (unsafeFromBuiltinData redeemer)
 
@@ -49,8 +71,52 @@ validator :: Scripts.Validator
 -- validator = Scripts.mkValidatorScript $$(PlutusTx.compile [||wrapped||])
 validator = Scripts.mkValidatorScript $$(PlutusTx.compile [||wrappedWithoutContext||])
 
-serialized :: PlutusScript PlutusScriptV1
-serialized = PlutusScriptSerialised . BSS.toShort . BSL.toStrict . serialise $ validator
+-- serialized :: PlutusScript PlutusScriptV1
+-- serialized = PlutusScriptSerialised . BSS.toShort . BSL.toStrict . serialise $ validator
 
 hash :: Scripts.ValidatorHash
 hash = validatorHash validator
+
+----------------------------------------------
+
+data MathBountyDatum = MBD 
+                     { mbBounty    :: Integer
+                     , mbDeadline :: POSIXTime }  
+
+PlutusTx.makeIsDataIndexed ''MathBountyDatum [('MBD,0)]        -- At compile time write an instance of this data type (MyWonderFullRedeemer) on the IsData typeclass
+
+{-# INLINABLE mathBountyValidator  #-}
+mathBountyValidator :: MathBountyDatum -> Integer -> ScriptContext -> Bool
+mathBountyValidator datum x sContext = traceIfFalse "Wrong guess!" ((mbBounty datum) == x*x) &&
+                                       traceIfFalse "Deadline passed!" deadlineReached
+       where
+            somedatum :: MathBountyDatum 
+            somedatum = mustFindScriptDatum @MathBountyDatum (head (PlutusV2.txInfoOutputs info)) info
+
+            info :: Contexts.TxInfo
+            info = Contexts.scriptContextTxInfo sContext
+
+            deadlineReached :: Bool
+            deadlineReached = contains (to $ mbDeadline datum) $ PlutusV2.txInfoValidRange info
+                           -- (to $ mbDeadline datum) `contains` (txInfoValidRange info)
+
+
+
+
+
+
+bountyWrapped :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+bountyWrapped = wrap mathBountyValidator
+
+bountyValidator :: Scripts.Validator
+bountyValidator = Scripts.mkValidatorScript $$(PlutusTx.compile [||bountyWrapped||])                 
+
+bountyValHash :: Scripts.ValidatorHash
+bountyValHash = validatorHash bountyValidator                      
+
+bountyAddress :: Plutus.V1.Ledger.Api.Address
+bountyAddress = Address (ScriptCredential bountyValHash) Nothing                -- New function to derive the address, included in the Utils library
+--bountyAddress = scriptAddress valiator 
+
+serialized :: PlutusScript PlutusScriptV1
+serialized = PlutusScriptSerialised . BSS.toShort . BSL.toStrict . serialise $ bountyValidator
